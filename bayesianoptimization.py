f3fd35e786e7a44b8b4ff237d0f5f7b5b42fe2c1
Matthias H Hennig <mhhennig@gmail.com>
2019-07-30 00:30:29 +0100

    First working version

---
+++ bayesianoptimization.py
@@ -0,0 +1,69 @@
+from baseoptimization import BaseOptimization
+# import spiketoolkit as st
+# import spikeextractors as se
+# import numpy as np
+# import logging
+# import pickle
+# from contextlib import redirect_stdout
+# from multiprocessing import Process, Queue, cpu_count
+
+# import psutil
+import matplotlib.pyplot as plt
+# from tqdm import tqdm
+import time
+# import os, sys
+
+from skopt import gp_minimize
+from skopt.space import Integer, Real, Categorical
+from skopt.plots import plot_convergence, plot_evaluations, plot_objective
+
+
+
+class BayesianOptimization(BaseOptimization):
+    def __init__(self,sorter,recording,gt_sorting, params_to_opt,space=None,run_schedule=[150,100],metric ='accuracy',recdir=None,
+                 outfile='results'):
+        BaseOptimization.__init__(self, sorter=sorter, recording=recording, gt_sorting=gt_sorting,
+                                  params_to_opt=params_to_opt,space=space,run_schedule=run_schedule,metric =metric,recdir = None, outfile=outfile)
+        self.results_obj = None
+
+    def run(self):
+        results = self.optimise(
+                self.params_to_opt, self.function_wrapper, self.run_schedule)
+        best_parameters = {key: results.x[i] for i, key in enumerate(self.params_to_opt)}
+        results['optimal_params'] = best_parameters
+        results.specs['args']['func'] = None
+        self.save_results(results, self.outfile)
+        self.results_obj = results
+            
+    def optimise(self, parameter_definitions, function, run_schedule):
+        # Parse parameter definitions to a list of skopt dimensions
+        dimensions = []
+        for (name, rang) in parameter_definitions.items():
+            if type(rang[1]) is int and type(rang) is not list:
+                dimensions.append(Integer(low=rang[0], high=rang[1], name=name))
+            if type(rang[1]) is float  and type(rang) is not list:
+                dimensions.append(Real(low=rang[0], high=rang[1], name=name))
+            if type(rang) is list:
+                dimensions.append(Categorical(rang, name=name))
+        run_schedule = self.run_schedule
+        start_time = time.time()
+    
+        results_object = gp_minimize(function,
+                                     dimensions,
+                                     acq_func='EI',
+                                     noise=1e-10,
+                                     n_calls=run_schedule[0],
+                                     n_random_starts=run_schedule[1],
+                                     n_jobs=-1,
+                                     verbose=True
+                                     )
+        print("--- %s seconds ---" % (time.time() - start_time))
+        return results_object
+    
+    def plot_convergence(self):
+        if self.results_obj is not None:
+            parameter_names = list(self.params_to_opt.keys())
+
+            plt.figure(figsize=(15,3.5))
+            plot_convergence(self.results_obj, ax=plt.gca())
+
---
+++ tpeoptimization.py
@@ -0,0 +1,72 @@
+from baseoptimization import BaseOptimization
+# import spiketoolkit as st
+# import spikeextractors as se
+# import numpy as np
+# import logging
+# import pickle
+# from contextlib import redirect_stdout
+# from multiprocessing import Process, Queue, cpu_count
+
+# import psutil
+import matplotlib.pyplot as plt
+# from tqdm import tqdm
+import time
+# import os, sys
+from collections import defaultdict
+
+import hyperopt
+from hyperopt.pyll.stochastic import sample
+from hyperopt import Trials
+from hyperopt import tpe
+from hyperopt import hp
+
+
+
+class TPEOptimization(BaseOptimization):
+    def __init__(self,sorter,recording,gt_sorting, params_to_opt,space,run_schedule=[100],metric ='accuracy',recdir=None,
+                 outfile='results'):
+        BaseOptimization.__init__(self, sorter=sorter, recording=recording, gt_sorting=gt_sorting, params_to_opt=params_to_opt, space=space, run_schedule=run_schedule, metric=metric, recdir = None, outfile=outfile)
+        self.trials = Trials()
+            
+    def run(self):
+        results = self.optimise(
+                self.params_to_opt, self.function_wrapper, self.run_schedule)
+        self.save_results(results, self.outfile)
+            
+    def optimise(self, parameter_definitions, function, run_schedule):
+        start_time = time.time()
+        best = hyperopt.fmin(function, self.space, algo=tpe.suggest, max_evals = run_schedule[0], trials=self.trials)
+        results_obj = defaultdict()
+        results_obj['optimal_params']=self.trials.best_trial['misc']['vals']
+        results_obj['best_accuracy']=-self.trials.best_trial['result']['loss']
+        print("--- %s seconds ---" % (time.time() - start_time))
+        return results_obj
+        
+    def plot_convergence(self):
+        
+        ys = [t['result']['loss'] for t in self.trials.trials]
+        
+        plt.figure(figsize=(15,3.5))
+        ax = plt.gca()
+        ax.grid()
+        n_calls = len(ys)
+        mins = [np.min(ys[:i]) for i in range(1, n_calls + 1)]
+        ax.plot(range(1, n_calls + 1), mins, c='b', marker=".", markersize=12, lw=2)
+        plt.xlabel('n_calls')
+        plt.ylabel('min(-Accuracy)')
+        plt.title('Convergence of TPE in sorting optimisation')
+        
+    def plot_histograms(self):
+        
+        parameters = list(trials.trials[0]['misc']['vals'].keys())
+        n = len(parameters)
+        cmap = plt.cm.jet
+        for i, val in enumerate(parameters):
+            xs = np.array([t['misc']['vals'][val] for t in self.trials.trials]).ravel()
+            ys = [-t['result']['loss'] for t in trials.trials]
+  
+            ys = np.array(ys)
+            plt.figure(figsize=(3,3))
+            plt.hist(xs)
+            plt.title(val)
+
